3a8de27201399eabf6698510f7e13a8b6def57d3
Mark <mark@remarkablemark.org>
2023-05-26 19:07:00 -0400

    feat(pages): create NotFound

---
+++ src/pages/NotFound/NotFound.test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from 'test/helpers';
+
+import NotFound from './NotFound';
+
+it('renders not found', () => {
+  renderWithProviders(<NotFound />);
+  expect(
+    screen.getByRole('heading', {
+      level: 1,
+      name: 'Not Found',
+    })
+  ).toBeInTheDocument();
+});
+
+it('renders home link', () => {
+  renderWithProviders(<NotFound />);
+  expect(
+    screen.getByRole('link', {
+      name: 'home',
+    })
+  ).toHaveAttribute('href', '/');
+});
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,24 @@
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { Link as RouterLink } from 'react-router-dom';
+import { useSetDocumentTitle } from 'src/hooks';
+
+export default function NotFound() {
+  useSetDocumentTitle('Not Found');
+
+  return (
+    <>
+      <Typography component="h1" paragraph variant="h4">
+        Not Found
+      </Typography>
+
+      <Typography paragraph>
+        Go to{' '}
+        <Link component={RouterLink} to="/">
+          home
+        </Link>
+        .
+      </Typography>
+    </>
+  );
+}
