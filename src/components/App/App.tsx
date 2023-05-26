5451f44bc151c58d1277d53ca3cb4fd05fa078cf
Mark <mark@remarkablemark.org>
2023-05-25 21:41:15 -0400

    feat(components): customize App theme so primary color is red

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,6 @@
+import { red } from '@mui/material/colors';
 import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { StrictMode } from 'react';
 import { Provider } from 'react-redux';
 import {
@@ -11,13 +13,23 @@ import { store } from 'src/store';
 
 const router = createBrowserRouter(createRoutesFromElements(routes));
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: red[900],
+    },
+  },
+});
+
 export default function App() {
   return (
     <StrictMode>
       <CssBaseline />
-      <Provider store={store}>
-        <RouterProvider router={router} />
-      </Provider>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ThemeProvider>
     </StrictMode>
   );
 }
