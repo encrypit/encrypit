e4da2e3ec43bef6f64e6e00a40a3e89ec045c7cd
Mark <mark@remarkablemark.org>
2023-06-08 20:52:03 -0400

    refactor(utils): tidy blobToBase64 data helper

--- src/utils/data.test.ts
+++ src/utils/data.test.ts
@@ -5,7 +5,5 @@ it('converts blob to base64', async () => {
   const blob = new Blob([text]);
   const base64 = await blobToBase64(blob);
   expect(base64).toContain('base64,' + btoa(text));
-  expect(base64).toMatchInlineSnapshot(
-    `"data:application/octet-stream;base64,dGV4dA=="`
-  );
+  expect(base64).toBe('data:application/octet-stream;base64,dGV4dA==');
 });
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,31 @@
+enum Events {
+  error = 'error',
+  loadend = 'loadend',
+}
+
+/**
+ * Converts Blob to Base64.
+ *
+ * @param blob - Blob.
+ * @returns - Base64.
+ */
 export async function blobToBase64(blob: Blob): Promise<string> {
-  const reader = new FileReader();
-  return new Promise((resolve) => {
-    reader.addEventListener('loadend', () => resolve(reader.result as string));
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    function loadend() {
+      resolve(reader.result as string);
+      reader.removeEventListener(Events.loadend, loadend);
+    }
+    reader.addEventListener(Events.loadend, loadend);
+
+    /* istanbul ignore next */
+    function error() {
+      reject(reader.error);
+      reader.removeEventListener(Events.error, error);
+    }
+    reader.addEventListener(Events.error, error);
+
     reader.readAsDataURL(blob);
   });
 }
