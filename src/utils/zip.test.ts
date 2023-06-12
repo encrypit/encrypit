c96e4a34638f30f95f5697f5b69e4c89dd1462fb
Mark <mark@remarkablemark.org>
2023-06-11 23:10:50 -0400

    feat(utils): increase zip password length via key stretching

--- src/utils/zip.test.ts
+++ src/utils/zip.test.ts
@@ -80,7 +80,8 @@ describe('unzip', () => {
     expect(zip.BlobWriter).toBeCalledTimes(1);
     expect(zip.ZipReader).toBeCalledTimes(1);
     expect(zip.ZipReader).toBeCalledWith(expect.anything(), {
-      password,
+      password:
+        'password:cGFzc3dvcmQ=:5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8:5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8:a8b64babd0aca91a59bdbb7761b421d4f2bb38280d3a75ba0f21f2bebc45583d446c598660c94ce680c47d19c30783a7:b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86',
     });
     expect(mockZipReader.getEntries).toBeCalledTimes(1);
     expect(mockZipReader.close).toBeCalledTimes(1);
--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -1,4 +1,5 @@
 import { BlobReader, BlobWriter, ZipReader, ZipWriter } from '@zip.js/zip.js';
+import { digest } from 'pepto';
 import { MIME } from 'shared/constants';
 
 const zipWriterOptions = {
@@ -19,14 +20,16 @@ async function zip(files: File[], password?: string): Promise<Blob> {
     ...zipWriterOptions,
     password,
   });
+
   await Promise.all(
     files.map((file) => zipWriter.add(file.name, new BlobReader(file)))
   );
+
   return zipWriter.close();
 }
 
 /**
- * Creates encrypted zip file.
+ * Creates a zip file and then encrypts it.
  *
  * @param files - Files.
  * @param password - Password.
@@ -38,11 +41,12 @@ export async function createZipFile(
 ): Promise<Blob> {
   const blob = await zip(files);
   const file = new File([blob], 'Archive.zip', { type: MIME.ZIP });
-  return zip([file], password);
+
+  return zip([file], await stretchPassword(password));
 }
 
 /**
- * Unzips encrypted zip file.
+ * Decrypts zip file.
  *
  * @param file - Zip file.
  * @param password - Password.
@@ -51,9 +55,28 @@ export async function createZipFile(
 export async function unzip(file: Blob, password: string): Promise<Blob> {
   const blobReader = new BlobReader(file);
   const blobWriter = new BlobWriter();
-  const zipReader = new ZipReader(blobReader, { password });
+  const zipReader = new ZipReader(blobReader, {
+    password: await stretchPassword(password),
+  });
+
   const [firstFile] = await zipReader.getEntries();
   const data = await firstFile.getData!(blobWriter);
   await zipReader.close();
   return data;
+}
+
+/**
+ * Increases password length via key stretching.
+ *
+ * @param password - Password.
+ * @returns - Stretched password.
+ */
+async function stretchPassword(password: string): Promise<string> {
+  const hashes = await Promise.all(
+    (['SHA-1', 'SHA-256', 'SHA-384', 'SHA-512'] as const).map((algorithm) =>
+      digest(algorithm, password)
+    )
+  );
+
+  return [password, btoa(password)].concat(hashes).join(':');
 }
