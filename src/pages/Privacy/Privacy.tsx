d68bea624ee4c69b2c75e1c049fe5876cd9d9c4a
Mark <mark@remarkablemark.org>
2023-07-03 23:28:10 -0400

    refactor(docs): move privacy.md from pages to docs

--- src/pages/Privacy/Privacy.tsx
+++ src/pages/Privacy/Privacy.tsx
@@ -1,8 +1,7 @@
+import privacy from 'docs/privacy.md?raw';
 import Markdown from 'src/components/Markdown';
 import { APP_NAME } from 'src/config';
 import { useSetDocumentTitle } from 'src/hooks';
-
-import privacy from './privacy.md?raw';
 
 export default function Privacy() {
   useSetDocumentTitle(`${APP_NAME} Privacy Policy`);
