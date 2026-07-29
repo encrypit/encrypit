4d75363a71ba9df3cfc0eadd80243acaf98531b8
Mark <mark@remarkablemark.org>
2026-07-29 15:38:39 -0400

    test(Dropzone): fix type in useOnDropRejected.test.ts

--- src/components/Dropzone/hooks/useOnDropRejected.test.ts
+++ src/components/Dropzone/hooks/useOnDropRejected.test.ts
@@ -1,4 +1,5 @@
 import { act, renderHook } from '@testing-library/react';
+import type { FileRejection } from 'react-dropzone';
 import { store, wrapper } from 'test/helpers';
 
 import { type OnDropRejected, useOnDropRejected } from './useOnDropRejected';
@@ -27,21 +28,18 @@ it('does not do anything if there are no file rejections', async () => {
 
 describe('file rejections', () => {
   const message = 'File is larger than 5242880 bytes';
-  let fileRejections: {
-    errors: {
-      code: string;
-      message: string;
-    }[];
-    file: File;
-  }[];
+  let fileRejections: FileRejection[];
   let onDropRejected: OnDropRejected;
 
   beforeEach(() => {
     fileRejections = Array(2)
       .fill(null)
-      .map((file) => ({
+      .map(() => ({
         errors: [{ code: 'too-many-files', message }],
-        file,
+        file: Object.assign(new File([], ''), {
+          path: '',
+          relativePath: '',
+        }),
       }));
     const { result } = renderHook(() => useOnDropRejected(), { wrapper });
     onDropRejected = result.current;
